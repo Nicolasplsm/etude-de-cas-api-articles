798c3077aede6b7a14bf6c5127e1426b09f17e4d
Nicolas Plasman <nicolasplasman.pro@gmail.com>
2025-11-23 13:06:33 +0100

    Étude de cas exercice 6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,15 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+
+      // Limite mémoire demandée
+      max_memory_restart: "200M",
+
+      // Fichiers de log
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",        // facultatif mais recommandé
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+
       env_production: {
         NODE_ENV: "production",
       },
